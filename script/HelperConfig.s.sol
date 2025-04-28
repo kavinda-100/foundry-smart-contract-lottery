8160c1fb6181edfd90d29e1ee0e1a0055c6e846c
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-28 20:36:10 +0530

    feat: add active network configuration management in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.25;
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
+    // struct to hold the configuration for the network
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -12,5 +13,34 @@ contract HelperConfig is Script {
         uint64 subscriptionId;
         uint32 callbackGasLimit;
         bool enableNativePayment;
+    }
+
+    NetworkConfig public activeNetworkConfig; // variable to hold the active network configuration
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30, // 30 seconds
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                subscriptionId: 0, // Replace with your subscription ID
+                callbackGasLimit: 500000, // Adjust as needed
+                enableNativePayment: false // Set to true if you want to accept native payments
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig; // Return the existing configuration if it exists
+        }
     }
 }
