ca9b55b941654565c1c3e6e59ad498c3f1fa42a1
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-28 10:08:28 +0530

    feat: implement enterRaffle function with entrance fee validation and player storage

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,14 +8,24 @@ pragma solidity ^0.8.25;
  * @dev The contract allows users to enter a raffle by sending Ether, and the winner is selected randomly.
  */
 contract Raffle {
+    // custom errors
+    error Raffle__NotEnoughETHEntered(); // Error for insufficient ETH sent
     // State variables
     uint256 private immutable i_entranceFee; // Fee to enter the raffle
+    address payable[] private s_players; // Array to store players' addresses
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee; // Set the entrance fee
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // Check if the sent value is less than the entrance fee
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered(); // Revert with custom error
+        }
+        // Add the player's address to the players array
+        s_players.push(payable(msg.sender)); // Store the player's address
+    }
 
     function pickWinner() public {}
 
